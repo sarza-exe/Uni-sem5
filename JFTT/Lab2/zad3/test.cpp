e5d31c1a1912aa8c610947dcdbce9a495c31a3d1
sarza-exe <sarazynd@gmail.com>
2025-11-13 15:30:17 +0100

    Rozwiązania listy 2

---
+++ JFTT/Lab2/zad3/test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/***/
+/** rtg */
+
+/*!vdfvf
+ * dfvf//\*
+ * dfvdf
+*/
+
+/// documentation
+///
+
+/// not documetation
+
+/*
+ comment
+*/
+
+/*comment* / */
+
+int main(void){
+    //comment
+    cout << "Pulapka \" \\
+                // a \
+                /* a */ \
+                "
+            << endl; //comment
+    cout << "//not a comment \
+    what up" <<endl;
+}
+
+
+/*
+Standard comment multine
+//! doxygen comment within standard multiline
+//! doxygen comment within standard multiline
+*/
